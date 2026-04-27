a0590bee34e50d25076d22c98d4bb2d50937c2cf
guk328 <guk328@lehigh.edu>
2026-04-27 17:24:38 -0400

    Finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Navbar />
 
-export default App
+      <section id="title" className="section">
+        <h1>Home</h1>
+      </section>
+
+      <section id="hooker" className="section">
+        <h2>About Us</h2>
+      </section>
+
+      <section id="members" className="section">
+        <h2>Members</h2>
+      </section>
+
+      <section id="calendar" className="section">
+        <h2>Calendar</h2>
+      </section>
+
+      <section id="design" className="section">
+        <h2>Programs</h2>
+      </section>
+    </>
+  );
+}
